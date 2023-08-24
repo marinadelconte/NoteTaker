6c840287a48bce1555eaa4c006aed89bea9ccd1a
Marina Del Conte <marinadelconte@yahoo.com>
2023-08-24 19:30:21 -0400

    added new note to page and db.json

--- server.js
+++ server.js
@@ -12,43 +12,35 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
-);
-
 app.get('/api/notes', (req, res) => {
-  res.json(`${req.method} request received`);
-  console.info(`${req.method} request received`);
 
+  console.info(`${req.method} request received`);
+  res.json(note);
 });
 
 app.post('/api/notes', (req, res) => {
   console.info(`${req.method} request received`);
 
-app.put()
 
-
-  const { note } = req.body;
-  
-  if (note) {
-     const newNote = {
-      title,
-      note_id: uuid(),
+  const newInput = req.body;
+console.log(newInput);
+  if (newInput) {
+    const newNote = {
+      title: newInput.title,
+      text: newInput.text,
+      id: uuid(),
     };
-    
-note.push(newNote)
+
+    note.push(newNote)
+    console.log(note);
     const noteString = JSON.stringify(note, null, 2);
 
-    fs.writeFile(`/db.json`, noteString, (err) =>
+    fs.writeFile(`./db/db.json`, noteString, (err) =>
       err
         ? console.error(err)
         : console.log(
-            `${newNote.title} has been written to JSON file`
-          )
+          `${newNote.title} has been written to JSON file`
+        )
     );
 
     const response = {
@@ -62,6 +54,14 @@ note.push(newNote)
     res.status(500).json('Error in adding new note');
   }
 });
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`Note Taking app listening at http://localhost:${PORT}`)
